79f6b25192ca784b6191b2b57f49c8ca672c1b93
KeiYamashita <yamashita.yr1@gmail.com>
2023-04-16 01:24:20 +0900

    for文拡張for文を使ったList

--- bread.java
+++ bread.java
@@ -6,15 +6,25 @@ public class bread {
     public static void main (String[] args) {
         List<String> bread =new ArrayList<>();
 
-        bread.add("メロンパン150円");
-        bread.add("クリームパン200円");
-        bread.add("カレーパン250円");
-        bread.add("クロワッサン180円");
+        bread.add("メロンパン");
+        bread.add("食パン");
+        bread.add("カレーパン");
+        bread.add("クロワッサン");
 
-        System.out.println(bread.get(0));
+        System.out.println("人気のパン４種類です。");
         System.out.println(bread.get(1));
         System.out.println(bread.get(2));
         System.out.println(bread.get(3));
+
+        System.out.println("人気のパン４種類です。");
+        for (int i = 0; i < bread.size(); i ++) {
+            System.out.println((bread.get(i)));
+        }
+
+        System.out.println("人気のパン４種類です。");
+        for (String str : bread) {
+            System.out.println(str);
+        }
     }
 }
 
