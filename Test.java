fb4d782217892e839b69e16bd547e6568590cf04
KeiYamashita <yamashita.yr1@gmail.com>
2023-04-17 08:42:36 +0900

    for文拡張for文を使ったList

--- Test.java
+++ Test.java
@@ -10,6 +10,7 @@ public class Test {
 
         System.out.println("第１回テニス大会の");
         System.out.println("順位発表です！");
+
         for (tennisPlayerList tennisPlayerList : TennisPlayerlist) {
             if (tennisPlayerList.getScore() == 4) {
                 System.out.println("第1位は" + tennisPlayerList.getScore() + "ゲーム獲得で" + tennisPlayerList.getAge() + "歳" + tennisPlayerList.getName());
