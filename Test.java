08e76828490aaa12c4ab84d57711f9ae9c0f0f0d
KeiYamashita <yamashita.yr1@gmail.com>
2023-04-16 09:05:22 +0900

    for文拡張for文を使ったList

--- Test.java
+++ Test.java
@@ -4,19 +4,19 @@ public class Test {
 
     public static void main(String[] args) {
 
-        List<TennisplayerList> TennisPlayerlist = List.of(new TennisplayerList("山下選手です", 10, 4),
-                new TennisplayerList("佐藤選手です", 11, 2),
-                new TennisplayerList("高橋選手です", 10, 1));
+        List<tennisPlayerList> TennisPlayerlist = List.of(new tennisPlayerList("山下選手です", 10, 4),
+                new tennisPlayerList("佐藤選手です", 11, 2),
+                new tennisPlayerList("高橋選手です", 10, 1));
 
         System.out.println("第１回テニス大会の");
         System.out.println("順位発表です！");
-        for (TennisplayerList tennisplayer : TennisPlayerlist) {
-            if (tennisplayer.getScore() == 4) {
-                System.out.println("第1位は" + tennisplayer.getScore() + "ゲーム獲得で" + tennisplayer.getAge() + "歳" + tennisplayer.getName());
-            } else if (tennisplayer.getScore() >= 2) {
-                System.out.println("第2位は" + tennisplayer.getScore() + "ゲーム獲得で" + tennisplayer.getAge() + "歳" + tennisplayer.getName());
-            } else if (tennisplayer.getScore() >= 1) {
-                System.out.println("第3位は" + tennisplayer.getScore() + "ゲーム獲得で" + tennisplayer.getAge() + "歳" + tennisplayer.getName());
+        for (tennisPlayerList tennisPlayerList : TennisPlayerlist) {
+            if (tennisPlayerList.getScore() == 4) {
+                System.out.println("第1位は" + tennisPlayerList.getScore() + "ゲーム獲得で" + tennisPlayerList.getAge() + "歳" + tennisPlayerList.getName());
+            } else if (tennisPlayerList.getScore() >= 2) {
+                System.out.println("第2位は" + tennisPlayerList.getScore() + "ゲーム獲得で" + tennisPlayerList.getAge() + "歳" + tennisPlayerList.getName());
+            } else if (tennisPlayerList.getScore() >= 1) {
+                System.out.println("第3位は" + tennisPlayerList.getScore() + "ゲーム獲得で" + tennisPlayerList.getAge() + "歳" + tennisPlayerList.getName());
             }
         }
     }
--- tennisPlayerList.java
+++ tennisPlayerList.java
@@ -1,10 +1,11 @@
-public class TennisplayerList {
+
+class tennisPlayerList {
 
     private String name;
     private int age;
     private int score;
 
-    public TennisplayerList(String name, int age, int score) {
+    public tennisPlayerList(String name, int age, int score) {
         this.name = name;
         this.age = age;
         this.score = score;
