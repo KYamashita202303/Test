20c340825147c219cab27f3b7c9b6e117072c1a4
KeiYamashita <yamashita.yr1@gmail.com>
2023-04-18 21:16:55 +0900

    変数名の大文字から小文字への修正 フォーマット設定

--- Test.java
+++ Test.java
@@ -4,19 +4,20 @@ public class Test {
 
     public static void main(String[] args) {
 
-        List<TennisPlayer> tennisPlayerList = List.of(new TennisPlayer(10, 4, "山下選手"), new TennisPlayer(11, 2, "高橋選手"), new TennisPlayer(10, 1, "佐藤選手"));
+        List<TennisPlayer> tennisPlayerList = List.of(new TennisPlayer(10, 4, "山下選手"),
+                new TennisPlayer(11, 2, "高橋選手"),
+                new TennisPlayer(10, 1, "佐藤選手"));
 
         System.out.println("第１回テニス大会の");
         System.out.println("順位発表です！");
 
-        for (TennisPlayer tennisPlayer : tennisPlayerList) {
-            if (tennisPlayer.getScore() == 4) {
-                System.out.println("第1位は" + tennisPlayer.getScore() + "ゲーム獲得で" + tennisPlayer.getAge() + "歳" + tennisPlayer.getName() + "です");
-            } else if (tennisPlayer.getScore() >= 2)
-                System.out.println("第2位は" + tennisPlayer.getScore() + "ゲーム獲得で" + tennisPlayer.getAge() + "歳" + tennisPlayer.getName() + "です");
-            else if (tennisPlayer.getScore() >= 1)
-                System.out.println("第3位は" + tennisPlayer.getScore() + "ゲーム獲得で" + tennisPlayer.getAge() + "歳" + tennisPlayer.getName() + "です");
-        }
+        for (TennisPlayer TennisPlayer : tennisPlayerList)
+            if (TennisPlayer.getScore() == 4) {
+                System.out.println("第1位は" + TennisPlayer.getScore() + "ゲーム獲得で" + TennisPlayer.getAge() + "歳" + TennisPlayer.getName() + "です");
+            } else if (TennisPlayer.getScore() >= 2)
+                System.out.println("第2位は" + TennisPlayer.getScore() + "ゲーム獲得で" + TennisPlayer.getAge() + "歳" + TennisPlayer.getName() + "です");
+              else if (TennisPlayer.getScore() >= 1)
+                System.out.println("第3位は" + TennisPlayer.getScore() + "ゲーム獲得で" + TennisPlayer.getAge() + "歳" + TennisPlayer.getName() + "です");
     }
 }
 
